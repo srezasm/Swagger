3834f034b293f36792a67f1bdc23adf4c2f2457e
SReza SMohseni <srezas.2017@gamil.com>
2020-02-05 23:05:53 +0330

    .gitingnore added

--- Controllers/MyApiController.cs
+++ Controllers/MyApiController.cs
@@ -5,42 +5,35 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
-namespace Swagger.Controllers
-{
-    [Route("api/[controller]")]
-    [ApiController]
-    public class MyApiController : ControllerBase
-    {
-        // GET: api/MyApi
-        [HttpGet]
-        public IEnumerable<string> Get()
-        {
-            return new string[] { "value1", "value2" };
-        }
+namespace Swagger.Controllers {
+   [Route("api/[controller]")]
+   [ApiController]
+   public class MyApiController : ControllerBase {
+      // GET: api/MyApi
+      [HttpGet]
+      public IEnumerable<string> Get() {
+         return new string[] { "value1", "value2" };
+      }
 
-        // GET: api/MyApi/5
-        [HttpGet("{id}", Name = "Get")]
-        public string Get(int id)
-        {
-            return "value";
-        }
+      // GET: api/MyApi/5
+      [HttpGet("{id}", Name = "Get")]
+      public string Get(int id) {
+         return "value";
+      }
 
-        // POST: api/MyApi
-        [HttpPost]
-        public void Post([FromBody] string value)
-        {
-        }
+      // POST: api/MyApi
+      [HttpPost]
+      public void Post([FromBody] string value) {
+      }
 
-        // PUT: api/MyApi/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
-        {
-        }
+      // PUT: api/MyApi/5
+      [HttpPut("{id}")]
+      public void Put(int id, [FromBody] string value) {
+      }
 
-        // DELETE: api/ApiWithActions/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
-        }
-    }
+      // DELETE: api/ApiWithActions/5
+      [HttpDelete("{id}")]
+      public void Delete(int id) {
+      }
+   }
 }
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Swagger
-This is a project about usage of swagger in asp.net core
+This is a project about usage of swagger in asp.net core 3.1
